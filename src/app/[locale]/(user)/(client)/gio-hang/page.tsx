68ea26ca2ec8604beac897f05fe43968cbbe4aae
Nguyen Minh Tuan <minhtuanng12@gmail.com>
2026-02-09 17:10:57 +0700

    initial cart

--- src/app/[locale]/(user)/(client)/gio-hang/page.tsx
+++ src/app/[locale]/(user)/(client)/gio-hang/page.tsx
@@ -1,8 +1,21 @@
 "use client"
 
+import Loading from "@/components/Loading"
 import { useCart } from "@/hooks/user/use-cart"
+import { Flex, Input, Space } from "antd"
+import { useTranslations } from "next-intl"
+import { Suspense } from "react"
 
 export default function GioHang() {
     const { cart } = useCart()
-    return <div></div>
+    const t = useTranslations();
+
+    return <Suspense fallback={<Loading />}>
+        <Space className="mt-5">
+            <Flex vertical gap={40}>
+                <Input placeholder={t('common.shipAddress')} />
+                
+            </Flex>
+        </Space>
+    </Suspense>
 }
